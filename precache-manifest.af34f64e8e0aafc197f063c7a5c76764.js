319a4bc99b13bfb92a2774e321dcae078251ff28
Yulia Nikolaeva <y.nikolaeva@rndev.org>
2020-08-06 03:03:20 +0300

    Updates

---
+++ precache-manifest.af34f64e8e0aafc197f063c7a5c76764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ceed52d7553d9d4672d8a036615889",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "7e67cef38bc3661dd0e1",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.713195bd.chunk.css"
+  },
+  {
+    "revision": "f778468d2f50826bf6f3",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.fe14504a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.fe14504a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e67cef38bc3661dd0e1",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.f8c72037.chunk.js"
+  },
+  {
+    "revision": "2a04b3148ba5cdb0d8c7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.515ede54.js"
+  }
+]);
